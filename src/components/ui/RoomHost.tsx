182e3d609e911973319ee3adb3226c803e367d8b
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-12-20 17:01:39 -0300

    refactor: format eslint

--- src/components/ui/RoomHost.tsx
+++ src/components/ui/RoomHost.tsx
@@ -15,38 +15,54 @@ function RoomHost() {
     }
 
     return (
-        <div className={style.container}>
+        <>
             <Head>
                 <title>Host Sala</title>
                 <link rel="icon" href="/favicon.png" />
             </Head>
 
-            <div className={style.section}>
-                <div className={style.side}>
-                    <h3>Pré-visualização</h3>
-                    <video width="400" height="400" poster="/loading.gif" autoPlay={true} controls={false} muted={true} playsInline={true} ref={videoRef} />
-                </div>
+            <div className={style.container}>
 
-                <div className={style.side}>
-                    <h3>Painel de controles</h3>
+                <div className={style.section}>
+                    <div className={style.side}>
+                        <h3>Pré-visualização</h3>
+                        <br />
+                        <video className={style.video} 
+                               poster="/loading.gif" 
+                               autoPlay={true} 
+                               controls={false} 
+                               muted={true} 
+                               playsInline={true} 
+                               ref={videoRef} />
+                    </div>
 
-                    <p><b>RoomId:</b> {roomName}</p>
-                    <p><b>Em uso:</b> {beingUsed ? 'Sim' : 'Não'}</p>
-                    {beingUsed ? <p><b>Usuário:</b> samuel.silva@elgin.com.br</p> : ''}
+                    <div className={style.side}>
+                        <h3>Painel de controles</h3>
+                        <br />
 
-                    <label><b>Câmera:</b> </label>
-                    <select value={selectedDevice} onChange={(e) => setSelectedDevice(e.target.value)}>
-                        <option value="" disabled>Selecione uma câmera</option>
-                        {devices.map(device => <option key={device.deviceId} value={device.deviceId}>{device.label}</option>)}
-                    </select><br />
+                        <p><b>RoomId:</b> {roomName}</p>
+                        <p><b>Em uso:</b> {beingUsed ? 'Sim' : 'Não'}</p>
+                        {beingUsed ? <p><b>Usuário:</b> samuel.silva@elgin.com.br</p> : ''}
 
-                    <label><b>Descrição da sala:</b> </label>
-                    <input id="description" type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
-                    <p>Exemplo: Stellar XL3, Stellar L3@ e Stellar XXXL3</p>
-                    <button onClick={() => handle()}>Atualizar descrição</button>
+                        <label><b>Câmera:</b> </label>
+                        <select value={selectedDevice} onChange={(e) => setSelectedDevice(e.target.value)}>
+                            <option value="" disabled>Selecione uma câmera</option>
+                            {devices.map(device => <option key={device.deviceId} value={device.deviceId}>{device.label}</option>)}
+                        </select><br />
+
+                        <label><b>Descrição da sala:</b> </label>
+                        <input id="description" 
+                               type="text" 
+                               value={description} 
+                               className={style.input}
+                               onChange={(e) => setDescription(e.target.value)} 
+                        />
+                        <p>Exemplo: Stellar XL3, Stellar L3@ e Stellar XXXL3</p>
+                        <button onClick={() => handle()}>Atualizar descrição</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
