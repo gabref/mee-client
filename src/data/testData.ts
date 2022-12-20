08c338edd3ae6f6321aad521c8bb8e524cb13d0e
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-12-20 08:33:03 -0300

    test: file to test input that will come from sockets

---
+++ src/data/testData.ts
@@ -0,0 +1,44 @@
+export const myrooms = [
+    {
+        roomId: '',
+        description: 'olá',
+        preview: 'conjunto-1',
+        available: false
+    },
+    {
+        roomId: '',
+        description: 'olá',
+        preview: 'conjunto-2',
+        available: false
+    },
+    {
+        roomId: '',
+        description: 'olá',
+        preview: 'conjunto-1',
+        available: false
+    },
+    {
+        roomId: '',
+        description: 'olá',
+        preview: 'conjunto-2',
+        available: false
+    },
+    {
+        roomId: '',
+        description: 'olá',
+        preview: 'conjunto-1',
+        available: false
+    },
+    {
+        roomId: '',
+        description: 'olá',
+        preview: 'conjunto-2',
+        available: false
+    },
+    {
+        roomId: '',
+        description: 'olá',
+        preview: 'conjunto-1',
+        available: false
+    },
+]
