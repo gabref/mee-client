fb451c4424b6cd9c9ec0a259da6cd17192c5075f
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-12-20 08:32:00 -0300

    feat: add room host component

---
+++ src/components/ui/RoomHost.tsx
@@ -0,0 +1,53 @@
+import Head from 'next/head'
+import { useRef, useState } from 'react'
+import style from './RoomHost.module.css'
+
+function RoomHost() {
+    const [roomName, setRoomName] = useState<string | string[]>('')
+    const [selectedDevice, setSelectedDevice] = useState('')
+    const [devices, setDevices] = useState<MediaDeviceInfo[]>([])
+    const videoRef = useRef<HTMLVideoElement>(null)
+    const [beingUsed, setBeingUsed] = useState(false)
+    const [description, setDescription] = useState('')
+
+    function handle() {
+        // socketIo.emit('description', roomName, localStorage.getItem('token'), description)}
+    }
+
+    return (
+        <div className={style.container}>
+            <Head>
+                <title>Host Sala</title>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+
+            <div className={style.section}>
+                <div className={style.side}>
+                    <h3>Pré-visualização</h3>
+                    <video width="400" height="400" poster="/loading.gif" autoPlay={true} controls={false} muted={true} playsInline={true} ref={videoRef} />
+                </div>
+
+                <div className={style.side}>
+                    <h3>Painel de controles</h3>
+
+                    <p><b>RoomId:</b> {roomName}</p>
+                    <p><b>Em uso:</b> {beingUsed ? 'Sim' : 'Não'}</p>
+                    {beingUsed ? <p><b>Usuário:</b> samuel.silva@elgin.com.br</p> : ''}
+
+                    <label><b>Câmera:</b> </label>
+                    <select value={selectedDevice} onChange={(e) => setSelectedDevice(e.target.value)}>
+                        <option value="" disabled>Selecione uma câmera</option>
+                        {devices.map(device => <option key={device.deviceId} value={device.deviceId}>{device.label}</option>)}
+                    </select><br />
+
+                    <label><b>Descrição da sala:</b> </label>
+                    <input id="description" type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+                    <p>Exemplo: Stellar XL3, Stellar L3@ e Stellar XXXL3</p>
+                    <button onClick={() => handle()}>Atualizar descrição</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RoomHost
