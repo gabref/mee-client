0f7839bd070b74b3bf74aa3f1020ac3716ed03f3
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-12-21 17:14:02 -0300

    feat: add auth context to login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,38 +1,30 @@
-import { FormEvent, useState } from 'react'
+import { cnpjMask, cpfMask } from '@utils/mask'
+import { FormEvent, useContext, useState } from 'react'
+import { AuthContext } from 'src/contexts/AuthContext'
 import style from '../styles/Login.module.css'
 
 export default function Login() {
     const [doc, setDoc] = useState('')
+    const { signIn } = useContext(AuthContext)
 
-    const handleDocumento = function(value: any)  {
-        value = value.replace(/[^0-9]/g, '')
-
-        if (value.length <= 11) {
-            value = value.match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/)
-            setDoc((!value[2] ? value[1] : value[1] + '.') + (!value[3] ? value[2] : value[2] + '.') + (!value[4] ? value[3] : value[3] + '-') + value[4])
+    function validate(value: string) {
+        if (value.length <= 14) {
+            value = cpfMask(value)
+            setDoc(value)
         } else {
-            value = value.match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/)
-            setDoc((!value[2] ? value[1] : value[1] + '.') + (!value[3] ? value[2] : value[2] + '.') + (!value[4] ? value[3] : value[3] + '/') + (!value[5] ? value[4] : value[4] + '-') + value[5])
+            value = cnpjMask(value)
+            setDoc(value)
         }
     }
 
-    const handleSubmit = async function(e: FormEvent) {
+    async function handleSignIn(e: FormEvent) {
         try {
             e.preventDefault()
     
-            await fetch('/api/auth', {
-                method: "POST",
-                body: JSON.stringify({
-                    doc: doc
-                }),
-                headers: {
-                    'Content-type': 'application/json;charset=utf-8'
-                }
-            })
-            // REDIRECIONAR E SALVAR EM UM CONTEXT O TOKEN
+            await signIn({ doc })            
         } catch (err) {
             // TODO
-            console.error(err)
+            console.error('Erro no handle Sign In' + err)
         }
     }
 
@@ -40,8 +32,8 @@ export default function Login() {
         <div className={style.loginContainer}>
             <h1>Login</h1>
             <h4 className={style.description}>Para entrar basta fazer login com seus dados da Elgin Developers Community</h4>
-            <form onSubmit={handleSubmit} className={style.formContainer}>
-                <input className={style.input} placeholder="CNPJ/CPF" type="text" maxLength={18} value={doc} onChange={(e) => handleDocumento(e.target.value)} required />
+            <form onSubmit={handleSignIn} className={style.formContainer}>
+                <input className={style.input} placeholder="CNPJ/CPF" type="text" maxLength={18} value={doc} onChange={(e) => validate(e.target.value)} required />
                 <div className={style.options}>
                     <button type="submit" className="button">Entrar na sala</button>
                     <span>ou</span>
