329f58870eedf76d464abda5ca0e76b77218e234
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-12-21 20:13:58 -0300

    feat: handling creation of new rooms as host

--- src/components/ui/RoomHostList.tsx
+++ src/components/ui/RoomHostList.tsx
@@ -1,21 +1,34 @@
 import style from './RoomHostList.module.css'
 import { lessRooms, myrooms } from '@data/testData'
 import RoomHost from './RoomHost'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 const loading = false
 
 function RoomHostList() {
     const [rooms, setRooms] = useState(lessRooms)
+    const [inputOk, setInputOk] = useState(true)
+    const [message, setMessage] = useState('')
+    const roomNameRef = useRef<HTMLInputElement>(null)
 
     function handleNewRoom() {
+        if (!roomNameRef.current?.value) {
+            setMessage('Escreva o nome da nova sala')
+            setInputOk(false)
+            return
+        }
+
         const newRoom = {
-            roomId: '',
+            roomId: roomNameRef.current.value,
             description: 'olá',
             preview: 'conjunto-1',
             available: false
         }
+
+        roomNameRef.current.value = ''
         setRooms(prevRooms => [...prevRooms, newRoom])
+        setInputOk(true)
+        return
     }
 
     return (
@@ -25,7 +38,13 @@ function RoomHostList() {
             )) : 'Carregando...'}            
 
             <div className={style.newRoomContainer}>
-                <input type="text" placeholder='Nome da Nova Sala' />
+                <input  
+                    type="text" 
+                    placeholder='Nome da Nova Sala' 
+                    className={`${inputOk ? null : style.input_error}`}
+                    ref={roomNameRef}
+                />
+                <p className={style.paragraph}>{message}</p>
                 <button className='button' onClick={handleNewRoom}>Criar Sala</button>
             </div>
         </div>
