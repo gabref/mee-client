c8f779d08dbf85ba08d77217730c81c2281c9f83
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-12-21 17:12:23 -0300

    feat: add auth as provider for application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@components/layouts/Layout'
+import { AuthProvider } from 'src/contexts/AuthContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <Layout >
-        <Component {...pageProps} />
-      </Layout>
+      <AuthProvider>
+        <Layout >
+          <Component {...pageProps} />
+        </Layout>
+      </AuthProvider>
   )
 }
