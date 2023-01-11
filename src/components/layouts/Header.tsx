9c8222d670309203231216c363d1379c761740f6
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-01-11 13:38:05 -0300

    feat: nav hamburger responsive

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,9 +1,18 @@
 import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
+import { useState } from "react"
 import style from './Header.module.css'
 
 function Header() {
+    const router = useRouter()
+    const [showNav, setShowNav] = useState(false)
+
+    function handleShowBar () {
+        setShowNav(!showNav)
+    }
+
     return (
         <header>
             <Head>
@@ -18,17 +27,44 @@ function Header() {
                         <Image src={'/elgin-logo.gif'} alt={'Elgin Logo'} width={94} height={40} />
                     </Link>
 
-                    <div className={style.navDesktop}>
+                    <input type="checkbox" className={style.menuIcon} onClick={handleShowBar}/>
+                    <div className={`${style.hamburgerLines}`}>
+                        <span className={`${style.line} ${style.line1}`}></span>
+                        <span className={`${style.line} ${style.line2}`}></span>
+                        <span className={`${style.line} ${style.line3}`}></span>
+                    </div>
+
+                    <div className={`${style.navDesktop} ${showNav && style.showNav}`}>
                         <ul>
-                            <li><Link href={'/'}>Início</Link></li>
-                            <li><a href={'/rooms'}>Salas</a></li>
-                            <li><Link href={'/jornada'}>Jornada</Link></li>
-                            <li><a href='https://github.com/ElginDeveloperCommunity' 
+                            <li className={router.pathname == '/' ? style.active : ''}>
+                                <Link href={'/'} >
+                                    Início
+                                </Link>
+                            </li>
+                            <li className={router.pathname == '/rooms' ? style.active : ''} >
+                                <a href={'/rooms'} >
+                                    Salas
+                                </a>
+                            </li>
+                            <li className={router.pathname == '/jornada' ? style.active : ''}>
+                                <Link href={'/jornada'} >
+                                    Jornada
+                                </Link>
+                            </li>
+                            <li>
+                                <a href='https://github.com/ElginDeveloperCommunity' 
                                    target={'_blank'} 
-                                   rel={'noopener noreferrer'}>GitHub</a></li>
-                            <li><a href='https://elgin.com.br/automacao/developers' 
+                                   rel={'noopener noreferrer'}>
+                                        GitHub
+                                </a>
+                            </li>
+                            <li>
+                                <a href='https://elgin.com.br/automacao/developers' 
                                    target={'_blank'} 
-                                   rel={'noopener noreferrer'}>Developers Community</a></li>
+                                   rel={'noopener noreferrer'}>
+                                    Developers Community
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </nav>
