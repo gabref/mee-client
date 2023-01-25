7ac86e7812cade107004713dfa3d0e0593216ea4
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-01-25 10:02:53 -0300

    fix: less stun servers

--- src/components/ui/RoomHost.tsx
+++ src/components/ui/RoomHost.tsx
@@ -34,9 +34,9 @@ function RoomHost({ room, socket, setRooms, peerConnections }:
                 urls: [
                     'stun:stun1.l.google.com:19302',
                     'stun:stun2.l.google.com:19302',
-                    'stun:stun.l.google.com:19302',
-                    'stun:stun3.l.google.com:19302',
-                    'stun:stun4.l.google.com:19302',
+                    // 'stun:stun.l.google.com:19302',
+                    // 'stun:stun3.l.google.com:19302',
+                    // 'stun:stun4.l.google.com:19302',
 
                 ]
             }
