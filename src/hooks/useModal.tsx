e47cf00d8501d0571ec78197b8f9c25ea5126143
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-02-17 14:16:06 -0300

    feat: added function to useModal for more reusable code

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,15 +1,28 @@
 import { useState } from 'react'
 
+type ModalParams = {
+    title: string, 
+    message: string, 
+    error?: boolean
+}
+
 export default function useModal() {
   const [isOpen, setIsOpen] = useState(false);
+  const [modal, setModal] = useState({ message: '', title: '', error: false })
 
   const toggleModal = () => {
     setIsOpen(!isOpen);
-  };
+  }
+
+  function handleToggleModal({ title, message, error = false }: ModalParams) {
+    setModal({ title, message, error })
+    toggleModal()
+  }
 
   return {
     isOpen,
-    setIsOpen,
-    toggleModal
+    toggleModal,
+    modal,
+    handleToggleModal
   };
 }
